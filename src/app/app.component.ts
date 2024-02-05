731cd4d37f650043f42cf1b92e2db7234ca2c9ef
giannis.botilias <john.botilias@gmail.com>
2024-02-05 15:50:53 +0200

    add toggle color scheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cocktail-app';
+  currentTheme: string | undefined;
+
+  constructor() {
+    const storedTheme = localStorage.getItem('currentTheme');
+    console.log(storedTheme);
+
+    if (storedTheme !== null) {
+      this.currentTheme = storedTheme;
+    } else {
+      this.currentTheme = 'light';
+      localStorage.setItem('currentTheme', this.currentTheme);
+    }
+
+    this.toggleTheme();
+
+  }
+
+  ngOnInit(): void {
+    // this.isLightTheme = true;
+  }
+
+  toggleTheme() {
+    if (this.currentTheme === 'light') {
+      document.body.classList.remove('dark-theme');
+      document.body.classList.add(`light-theme`);
+      this.currentTheme = 'dark'; 
+      localStorage.setItem('currentTheme', 'light');
+    } else {
+      document.body.classList.remove('light-theme');
+      document.body.classList.add(`dark-theme`);
+      this.currentTheme = 'light';
+      localStorage.setItem('currentTheme', 'dark');
+    }
+  }
+  
 }
