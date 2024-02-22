81ab8dcb46108d15dbfa23e8c6d9e1cdacaf985d
giannis.botilias <john.botilias@gmail.com>
2024-02-22 16:09:41 +0200

    fix bugs

--- src/app/item/item-list/item-list.component.ts
+++ src/app/item/item-list/item-list.component.ts
@@ -10,7 +10,7 @@ import { ItemModel } from 'src/app/_models/item-model';
 export class ItemListComponent implements OnInit {
 
   public itemList: ItemModel[] = [];
-  filteredItemList: any[] = [];
+  filteredItemList: ItemModel[] = [];
   searchByName: string = '';
   categories: string[] = [];
   selectedCategory: string = '';
@@ -21,18 +21,24 @@ export class ItemListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    // Fetch data from the resolved route and initialize components
     this.activatedRoute.data.subscribe((response: any) => {
       this.itemList = response.data.drinks;
+      // Make a copy of the itemList for filtering
       this.filteredItemList = [...this.itemList];
+      // Get unique categories from the itemList
       this.categories = this.getCategories();
-      this.selectCategory();
+      // Initial filter to display items
+      this.filterItems();
     }, error => {
       console.log(error);
     })
   }
 
+  // Method to get unique categories
   getCategories(): string[] {
     const allCategories: string[] = [];
+    // Iterate throw itemList
     this.itemList.forEach((item) => {
       if (!allCategories.includes(item.strCategory)) {
         allCategories.push(item.strCategory);
@@ -41,34 +47,31 @@ export class ItemListComponent implements OnInit {
     return allCategories;
   }
 
-  searchItemByName(): void { 
-    if (this.selectedCategory !== '') {
-      this.filteredItemList = this.itemList.filter(item =>
-        item.strDrink.toLowerCase().includes(this.searchByName.toLowerCase()) &&
-        item.strCategory === this.selectedCategory
-      );
-    } else {
-      this.filteredItemList = this.itemList.filter(item =>
-        item.strDrink.toLowerCase().includes(this.searchByName.toLowerCase())
-      );
-    }
+  // Method to filter items based on selected category and search criteria
+  filterItems(): void {
+    this.filteredItemList = this.itemList.filter(item =>
+      // Check if the selected category matches and if the item name contains the search term
+      (!this.selectedCategory || item.strCategory === this.selectedCategory) &&
+      item.strDrink.toLowerCase().includes(this.searchByName.toLowerCase())
+    );
   }
 
-  selectCategory(): void {
-    if (this.selectedCategory !== '') {
-      this.filteredItemList = this.itemList.filter(item =>
-        item.strCategory === this.selectedCategory &&
-        item.strDrink.toLowerCase().includes(this.searchByName.toLowerCase())  
-      );
-    } else {
-      this.filteredItemList = this.itemList.filter(item =>
-        item.strDrink.toLowerCase().includes(this.searchByName.toLowerCase())
-      );
-    }
+  // Method to handle search input changes
+  onSearchChange(searchTerm: string): void {
+    // Update the search term and trigger filtering
+    this.searchByName = searchTerm;
+    this.filterItems();
   }
 
+  // Method to handle category selection changes
+  onCategoryChange(selectedCategory: string): void {
+    // Update the selected category and trigger filtering
+    this.selectedCategory = selectedCategory;
+    this.filterItems();
+  }
+
+  // Method to navigate to item details
   showItemDetails(itemId: string): void {
-    console.log(itemId);
     this.router.navigate(['item-detail'], { queryParams: { _id: itemId } })
   }
 
